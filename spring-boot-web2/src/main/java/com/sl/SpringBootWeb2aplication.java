5f392061ac378f0ddc78e1d1cf1318e95e2a6fc4
shuliangzhao <809894780@qq.com>
2019-07-21 17:02:24 +0800

    springboot整合web

--- spring-boot-web2/src/main/java/com/sl/SpringBootWeb2aplication.java
+++ spring-boot-web2/src/main/java/com/sl/SpringBootWeb2aplication.java
@@ -2,8 +2,10 @@ package com.sl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@ServletComponentScan
 public class SpringBootWeb2aplication {
 
     public static void main(String[] args) {
