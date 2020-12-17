90b1f2b37b77a60de47fb828fdb683982c4c84c2
shuliangzhao <809894780@qq.com>
2020-12-17 21:25:52 +0800

    mybatis

---
+++ spring-boot-mybatis/src/main/java/com/sl/mapper/BookMapper.java
@@ -0,0 +1,20 @@
+package com.sl.mapper;
+
+
+import com.sl.entity.User;
+
+import java.util.List;
+
+/**
+ * @author shuliangzhao
+ * @Title: BookMapper
+ * @ProjectName spring-boot-learn
+ * @Description: TODO
+ * @date 2019/7/23 23:03
+ */
+public interface BookMapper {
+
+    List<User> getUser();
+
+    int insert(User user);
+}
