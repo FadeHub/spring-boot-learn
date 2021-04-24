444d048ab2a49f5f50738a7e5d39fcc9ca8009b1
shuliangzhao <809894780@qq.com>
2021-04-24 09:15:07 +0800

    删除依赖

--- spring-boot-springbatch/src/main/java/com/sl/classifier/CatClassifier.java
+++ spring-boot-springbatch/src/main/java/com/sl/classifier/CatClassifier.java
@@ -4,6 +4,10 @@ import com.sl.entity.Cat;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.classify.Classifier;
 
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
 /**
  * @author shuliangzhao
  * @Title: CatClassifier
@@ -20,6 +24,15 @@ public class CatClassifier implements Classifier<Cat, ItemProcessor<Cat,Cat>> {
     public CatClassifier(ItemProcessor<Cat, Cat> catNameUpperProcessor, ItemProcessor<Cat, Cat> catAddressUpperProcessor) {
         this.catNameUpperProcessor = catNameUpperProcessor;
         this.catAddressUpperProcessor = catAddressUpperProcessor;
+    }
+
+    public static void main(String[] args) {
+        List<String> list = Arrays.asList("1","12");
+        Iterator<String> iterator = list.iterator();
+        while (iterator.hasNext()) {
+            String next = iterator.next();
+            System.out.println(next);
+        }
     }
 
     @Override
