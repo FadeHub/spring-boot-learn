3613babb870d5abfb79e91fc9b9c39470ca11643
zhaoshuliang <809894780@qq.com>
2021-12-06 22:19:48 +0800

    mybatis 分页查询

---
+++ spring-boot-mybatis-plus/src/main/java/com/sl/config/MyBatisConfig.java
@@ -0,0 +1,24 @@
+package com.sl.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @package: com.sl.config
+ * @author: shuliangzhao
+ * @description:
+ * @date 2021/12/620:20
+ */
+@Configuration
+public class MyBatisConfig {
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+}
---
+++ spring-boot-mybatis-plus/src/main/java/com/sl/controller/SysController.java
@@ -0,0 +1,54 @@
+package com.sl.controller;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.sl.entity.SysUser;
+import com.sl.mapper.SysUserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * @author shuliangzhao
+ * @Title: SysController
+ * @ProjectName spring-boot-learn
+ * @Description: TODO
+ * @date 2021/5/20 15:45
+ */
+@RestController
+public class SysController {
+
+    @Autowired
+    private SysUserMapper sysUserMapper;
+
+    @GetMapping("/getSysUserByName")
+    public void  getSysUserByName() {
+        QueryWrapper<SysUser> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("username","xiaoming");
+        SysUser sysUser = sysUserMapper.selectOne(queryWrapper);
+        System.out.println(sysUser);
+    }
+
+    @GetMapping("/getPage")
+    public void  getPage() {
+        QueryWrapper<SysUser> queryWrapper = new QueryWrapper<>();
+        Page<SysUser> page = new Page();
+        page.setCurrent(2);
+        page.setSize(10);
+        Page<SysUser> page1 = sysUserMapper.selectPage(page, queryWrapper);
+        List<SysUser> records = page1.getRecords();
+        System.out.println(records);
+    }
+
+    @GetMapping("/insert")
+    public void insert() {
+        for (int i=1;i<100;i++) {
+            SysUser sysUser = new SysUser();
+            sysUser.setUsername("长伞");
+            sysUser.setPassword("123456");
+            sysUserMapper.insert(sysUser);
+        }
+    }
+}
