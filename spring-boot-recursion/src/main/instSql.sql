a3f704440af8aa51b73844093f635db2d48ddff7
zhaoshuliang <809894780@qq.com>
2021-12-12 21:10:04 +0800

    修改初始化sql

--- spring-boot-recursion/src/main/instSql.sql
+++ spring-boot-recursion/src/main/instSql.sql
@@ -5,11 +5,11 @@ CREATE TABLE `limit_mesh` (
      PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
-INSERT INTO `chapter01`.`limit_mesh`(`id`, `parent_node`, `child_node`) VALUES (1, 'crilm', 'inst');
-INSERT INTO `chapter01`.`limit_mesh`(`id`, `parent_node`, `child_node`) VALUES (2, 'crilm', 'cash');
-INSERT INTO `chapter01`.`limit_mesh`(`id`, `parent_node`, `child_node`) VALUES (3, 'crilm', 'instCash');
-INSERT INTO `chapter01`.`limit_mesh`(`id`, `parent_node`, `child_node`) VALUES (4, 'cust', 'crilm');
-INSERT INTO `chapter01`.`limit_mesh`(`id`, `parent_node`, `child_node`) VALUES (5, 'custTotal', 'cust');
-INSERT INTO `chapter01`.`limit_mesh`(`id`, `parent_node`, `child_node`) VALUES (6, 'cust', 'splmt');
-INSERT INTO `chapter01`.`limit_mesh`(`id`, `parent_node`, `child_node`) VALUES (7, 'splmt', 'byj');
-INSERT INTO `chapter01`.`limit_mesh`(`id`, `parent_node`, `child_node`) VALUES (8, 'splmt', 'ygj');
+INSERT INTO `limit_mesh`(`id`, `parent_node`, `child_node`) VALUES (1, 'crilm', 'inst');
+INSERT INTO `limit_mesh`(`id`, `parent_node`, `child_node`) VALUES (2, 'crilm', 'cash');
+INSERT INTO `limit_mesh`(`id`, `parent_node`, `child_node`) VALUES (3, 'crilm', 'instCash');
+INSERT INTO `limit_mesh`(`id`, `parent_node`, `child_node`) VALUES (4, 'cust', 'crilm');
+INSERT INTO `limit_mesh`(`id`, `parent_node`, `child_node`) VALUES (5, 'custTotal', 'cust');
+INSERT INTO `limit_mesh`(`id`, `parent_node`, `child_node`) VALUES (6, 'cust', 'splmt');
+INSERT INTO `limit_mesh`(`id`, `parent_node`, `child_node`) VALUES (7, 'splmt', 'byj');
+INSERT INTO `limit_mesh`(`id`, `parent_node`, `child_node`) VALUES (8, 'splmt', 'ygj');
